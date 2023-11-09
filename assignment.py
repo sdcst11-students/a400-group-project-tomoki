7df5248dd748fa0476df70fc51d18de00bb58123
Tomokim06 <tomokim048@deltalearns.ca>
2023-11-09 13:26:00 -0800

    a400

--- assignment.py
+++ assignment.py
@@ -2,7 +2,6 @@
 # Volume Calculator
 # Feel free to rename your variables
 
-
 def title():
     # Will display a title screen
     # input parameters: none needed
@@ -11,21 +10,20 @@ def title():
     # Modified:
     # title
     print("-= SUPER ULTRA CALCULATOR =- 2000\n")
-    q1 = ('Press "1" to see instructions')
-    q2 = ('Press "2" to exit calculator')
+    q1 = ('Press "1" to see instructions: ')
+    q2 = ('Press "2" to exit calculator: ')
     if input(q1) == 1:
         instructions()
     if input(q2) == 2:
-        exit()
-    return input
+        bye()
 
 def instructions():
     print('__________________________________\nYou will enter in a number that represents "x"')
-    print('Press "1" to find circumference of a circle where "x" is the radius')
-    print('Press "2" to find area of a circle where "x" is the radius')
-    print('Press "3" to find volume of a sphere where "x" is the radius')    
-    print('Press "4" to find surface area of a sphere where "x" is the radius')
-    print('Press "5" to find how many spheres would be needed to reach the moon from earth if the spheres were chaining together to make a line and "x" is the radius of a circle in meters')
+    print('Press "1" to find circumference of a circle where "x" is the radius: ')
+    print('Press "2" to find area of a circle where "x" is the radius: ')
+    print('Press "3" to find volume of a sphere where "x" is the radius: ')    
+    print('Press "4" to find surface area of a sphere where "x" is the radius: ')
+    print('Press "5" to find "x" to the power of "x": ')
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
@@ -33,6 +31,13 @@ def instructions():
     # Modified:
     return None
 
+def bye():
+    exit()
+
+
+#def five():
+    sol = x**x
+    return sol
 
 
 def main():
