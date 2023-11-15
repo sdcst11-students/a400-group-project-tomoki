2208009e0df1d2ce3f8c162f25167131f3209e5b
Tomokim06 <tomokim048@deltalearns.ca>
2023-11-15 13:34:10 -0800

    a400

--- assignment.py
+++ assignment.py
@@ -2,6 +2,7 @@
 # Volume Calculator
 # Feel free to rename your variables
 import math
+
 def title():
     # Will display a title screen
     # input parameters: none needed
@@ -19,67 +20,76 @@ def title():
         print("Invalid Input")
 
 def instructions():
-    while True:
-        print ('__________________________________')
-        q2 = ('Press "1" to find circumference of a circle where given the radius\nPress "2" to find area of a circle where given the the radius\nPress "3" to find volume of a sphere where given the radius\nPress "4" to find surface area of a sphere where given the radius\nPress "5" for exponent calculator\nType here -> ')
-        if input(q2) == "1":
-            one()
-        elif input(q2) == "2":
-            two()
-        elif input(q2) == "3":
-            three()
-        elif input(q2) == "4":
-            four()
-        elif input(q2) == "5":
-            five()
-        else:
-            print("Invalid Input")
+    print('__________________________________')
+    print('This calculator can do 5 tasks.\nThe options will be shown next')
+
+
+def menu():
+    print ('__________________________________')
+    print ('Press "1" to find circumference of a circle where given the radius')
+    print ('Press "2" to find area of a circle where given the the radius')
+    print ('Press "3" to find volume of a sphere where given the radius')
+    print ('Press "4" to find surface area of a sphere where given the radius')
+    print ('Press "5" to find largest square in a circle\nType here -> ')
             # Will display instructions
             # input parameters: none needed
             # output parameters: None
             # Author:
             # Modified:
-            return None
 
-def exit():
-    exit()
+def circumference(r):
+    return(r*2*math.pi)
 
-def one():
-    q = "What is the radius of your circle? "
-    circumference = float(input(q))*2*math.pi
-    print(f"The circumference of your circle is {circumference}")
+def area(r):
+    return((r**2*math.pi))
 
-def two():
-    q = "What is the radius of your circle? "
-    area = (float(input(q))**2)*math.pi
-    print(f"The area of your circle is {area}")  
+def volume(r):
+    return((r**3)*math.pi*(4/3))
 
-def three():
-    q = "What is the radius of your sphere? "
-    volume = (float(input(q))**3)*math.pi*(4/3)
-    print(f"The area of your circle is {volume}") 
+def surface_area(r):   
+    return (((r**2)*math.pi)*4)
 
-def four():   
-    q = "What is the radius of your sphere? "
-    sa = (float(input(q))**2)*math.pi*4
-    print(f"The surface area of your sphere is {sa}") 
-
-def five():
-    q = "What is the radius of your sphere? "
+def square_in_circle(r):
+    return ((r*(2**0.5))**2)
 
 
 def main():
-    """
-    main block of code that will run your program and control program flow
-    You will need to include a while loop to keep repeating the commands until
-    the user chooses to exit
-    """
-    title()
-    instructions()
-
     while True:
-        
-        # keep giving options to choose menu options until they choose to exit
+        choice = input("Enter choice(1/2/3/4/5): ")
+
+        if choice in ('1', '2', '3', '4', '5'):
+            try:
+                r = float(input("Enter first number: "))
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+                continue
+
+            if choice == '1':
+                print("The circumference of your circle is ",circumference(r))
+
+            elif choice == '2':
+                print("The area of your circle is ",area(r))
+
+            elif choice == '3':
+                print("The volume of your sphere is ", volume(r))
+
+            elif choice == '4':
+                print("The surface area of your sphere is ", surface_area(r))
+
+            elif choice == '5':
+                print(          
+
+            next_question = input("Let's do next calculation? (yes/no): ")
+            if next_question == "no":
+                break
+        else:
+            print("Invalid Input")
+
+
+
+
+
+        #keep giving options to choose menu options until they choose to exit
         #unMainMenu()
         #getInput()
         #use input to decide which function to run
@@ -87,3 +97,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
